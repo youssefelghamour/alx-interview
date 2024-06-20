1548af8f293db0e583020988aad2f55b45d0ca32
youssef <youssefelghamour@gmail.com>
2024-06-20 04:15:36 -0700

    0x03-log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,7 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ log parsing """
 import sys
-import re
+
+
+def print_logs(status_counts, total_size):
+    """ Print current statistics """
+    print(f"File size: {total_size}")
+    for code in sorted(status_counts.keys()):
+        if status_counts[code] > 0:
+            print(f"{code}: {status_counts[code]}")
+
 
 total_file_size = 0
 status_code_counts = {
@@ -16,47 +24,21 @@ status_code_counts = {
 }
 line_count = 0
 
-# Regular expression to match the input format
-log_pattern = re.compile(
-    r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) '  # IP address
-    r'- \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})\] '  # Date & Time
-    r'"GET /projects/260 HTTP/1\.1" '  # Request line
-    r'(\d{3}) '  # Status code
-    r'(\d{1,4})$'  # File size
-)
-
-
-def print_logs(total_size, status_counts):
-    """ prints logs """
-    print(f"File size: {total_size}")
-    for code in sorted(status_counts.keys()):
-        if status_counts[code] > 0:
-            print(f"{code}: {status_counts[code]}")
-
-
 try:
     for line in sys.stdin:
-        line = line.strip()
-        # Match the line with the regex pattern
-        match = log_pattern.match(line)
-        if match:
-            ip_address = match.group(1)
-            status_code = int(match.group(3))
-            file_size = int(match.group(4))
+        parsed_line = line.split()
+        parsed_line = parsed_line[::-1]
 
-            # Update total file size
-            total_file_size += file_size
+        if len(parsed_line) > 2:
+            line_count += 1
+            total_file_size += int(parsed_line[0])
+            status_code = int(parsed_line[1])
 
-            # Update status code counts if it's in the predefined set
             if status_code in status_code_counts:
                 status_code_counts[status_code] += 1
 
-            line_count += 1
-
-            # Print statistics every 10 lines
             if line_count == 10:
                 line_count = 0
-                print_logs(total_file_size, status_code_counts)
-except KeyboardInterrupt:
-    # Print logs on keyboard interruption
-    print_logs(total_file_size, status_code_counts)
+                print_logs(status_code_counts, total_file_size)
+finally:
+    print_logs(status_code_counts, total_file_size)
